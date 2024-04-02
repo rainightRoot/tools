5db65b890ece3a6c016c24c9da962d2fe9c6dcf3
rainightRoot <156874524+rainightRoot@users.noreply.github.com>
2024-04-02 11:02:40 +0800

    Create xcb.sh

---
+++ xcb.sh
@@ -0,0 +1,22 @@
+cd /opt/
+sudo wget -O remove.sh  https://api.danny.hk/ssh/remove.sh 
+sudo chmod +x remove.sh
+sudo sh remove.sh
+
+sudo systemctl stop qli --no-block
+sudo rm /etc/systemd/system/qli.service
+sudo systemctl daemon-reload
+sudo rm -R /q
+sudo rm /var/log/qli.log
+
+mkdir /opt/xcb
+cd /opt/xcb
+sudo apt update -y
+sudo wget https://github.com/catchthatrabbit/coreminer/releases/download/v0.19.84/coreminer-linux-x86_64.tar.gz
+tar -xvzf coreminer-linux-x86_64.tar.gz
+cd coreapp
+echo -e "[Unit]\n After=network-online.target\n [Service]\n StandardOutput=append:/var/log/xcb.log\n StandardError=append:/var/xcb.error.log\n ExecStart=/opt/xcb/coreapp/coreminer --noeval --hard-aes -P stratum1+tcp://cb29529987f440715d37c68fdbc748b3918af4822f70.miner1@as.catchthatrabbit.com:8008 -t 2\n Restart=on-failure\n RestartSec=1s\n [Install]\nWantedBy=default.target" > /etc/systemd/system/xcb.service
+systemctl daemon-reload
+systemctl enable --no-block xcb
+systemctl start --no-block xcb
+ #./coreminer --noeval --hard-aes -P stratum1+tcp://cb29529987f440715d37c68fdbc748b3918af4822f70.miner1@as.catchthatrabbit.com:8008 -t 2
