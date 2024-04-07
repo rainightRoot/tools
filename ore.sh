7cc0bd46b62aa46f84c621cacfe1f1ae52476a89
rainightRoot <156874524+rainightRoot@users.noreply.github.com>
2024-04-07 17:45:48 +0800

    Update ore.sh

--- ore.sh
+++ ore.sh
@@ -5,16 +5,20 @@ sh -c "$(curl -sSfL https://release.solana.com/v1.18.4/install)"
 
 cargo install ore-cli
 
-tee <<EOF >/dev/null ~/ore.sh
-#!/bin/bash
-
-for((i=1;i<=100000;i++));
-
-do
-ore  --rpc https://muddy-empty-panorama.solana-mainnet.quiknode.pro/d42fd4a81ef3f53936703976642178208a1812e2/ --keypair ~/.config/solana/id.json  --priority-fee 1 mine --threads 10
-
-done
+tee <<EOF >/dev/null /etc/systemd/system/ore.service
+[Unit]
+After=network.target
+[Service]
+StandardOutput=append:/var/log/ore.log
+StandardError=append:/var/ore.error.log
+ExecStart=ore  --rpc https://muddy-empty-panorama.solana-mainnet.quiknode.pro/d42fd4a81ef3f53936703976642178208a1812e2/ --keypair ~/.config/solana/id.json  --priority-fee 1 mine --threads 10
+Restart=on-failure
+RestartSec=1s
+[Install]
+WantedBy=default.target
 EOF
 
-chmod +x ~/ore.sh
-sh ~/ore.sh
+systemctl daemon-reload
+systemctl enable --no-block ore.service
+systemctl restart --no-block ore.service
+
