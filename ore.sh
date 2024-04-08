db515c9e8c3d722ee81870ccba671119ee9674a8
rainightRoot <156874524+rainightRoot@users.noreply.github.com>
2024-04-08 10:17:04 +0800

    Update ore.sh

--- ore.sh
+++ ore.sh
@@ -12,7 +12,7 @@ for((i=1;i<=100000;i++));
 
 do
 
-ore --rpc https://api.mainnet-beta.solana.com --keypair ~/.config/solana/id.json --priority-fee 1 mine --threads 10
+/root/.cargo/bin/ore --rpc https://api.mainnet-beta.solana.com --keypair ~/.config/solana/id.json --priority-fee 1 mine --threads 10
 
 done
 EOF
