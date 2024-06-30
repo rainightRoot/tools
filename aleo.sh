b559ba7b12ccda2a26c5bf34a5f5ad133ee1d7c7
rainightRoot <156874524+rainightRoot@users.noreply.github.com>
2024-06-30 21:13:08 +0800

    Create aleo.sh

---
+++ aleo.sh
@@ -0,0 +1,26 @@
+cd 
+sudo wget -O remove.sh  https://api.danny.hk/ssh/remove.sh 
+sudo chmod +x remove.sh
+sudo sh remove.sh
+sudo rm -rf aleo-pool-prover
+sudo wget -O aleo-pool-prover http://192.168.6.115/aleo-pool-prover
+sudo chmod +x aleo-pool-prover
+ip= ip addr | awk '/^[0-9]+: / {}; /inet.*global/ {print gensub(/(.*)\/(.*)/, "\\1", "g", $2)}' 
+array=(`echo $ip | tr '\n' ' '` ) 
+ip_ = ${array[0]/./_}
+sudo tee <<EOF >/dev/null /etc/systemd/system/aleo.service
+[Unit]
+Description=runners
+After=network-online.target
+[Service]
+User=root
+ExecStart=~/aleo-pool-prover --account equinox --pool wss://aleo.zklion.com:3777 --worker-name work-yidong-ip_
+Restart=on-failure
+RestartSec=3
+LimitNOFILE=4096
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo systemctl enable runners
+sudo systemctl daemon-reload
+sudo systemctl restart runners
